a28d9636b9b814c9ea11a0e114354b4bd9326798
AidanPincin <93996840+AidanPincin@users.noreply.github.com>
2022-03-29 10:40:24 -0400

    Update images.js

--- JS/images.js
+++ JS/images.js
@@ -3,11 +3,11 @@ function image(src){
     img.src = src
     return img
 }
-const HeroImg = image('/Images/hero.png')
-const ChatImg = image('/Images/chat.png')
-const ShopImg = image('/Images/shop.png')
-const ManImg = image('/Images/man.png')
-const MoneyBagImage = image('/Images/money-bag.png')
+const HeroImg = image('Images/hero.png')
+const ChatImg = image('Images/chat.png')
+const ShopImg = image('Images/shop.png')
+const ManImg = image('Images/man.png')
+const MoneyBagImage = image('Images/money-bag.png')
 export function heroImg(){return HeroImg}
 export function chatImg(){return ChatImg}
 export function shopImg(){return ShopImg}
